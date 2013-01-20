2c46c39b05e8977a26fbb998568e46487f6e41a8
Jimmy Li <contact@jimmyzmli.ca>
2013-01-20 15:37:58 -0500

    Added slug to test panels

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,12 @@
-import ui.base as ui
+import sys
+import ui.all as ui
 import defs as HRP
 
 win = ui.Window(HRP.WIN_TITLE)
+#sys.stdout = win.GetStdOut()
+tpane = ui.TextPanel(win, "text_1", "INTRO TO PROGRAM AAAAAAAAAAAAA"+"...... "*100)
+ttpane = ui.TextPanel(win, "text_2", "PANEL II")
+win.addPane(tpane)
+win.addPane(ttpane)
+win.show(tpane)
 ui.launch_win(win)

470ff19f1deb7375958b7864177f1e0966436c20
Kevin Xia <kevinxyc@qq.com>
2013-01-24 14:27:35 -0500

    search + delete

--- src/record.py
+++ src/record.py
@@ -2,22 +2,20 @@
 # Name:        module1
 # Purpose:
 #
-# Author:      301867487
+# Author:      Kevin
 #
 # Created:     14/01/2013
-# Copyright:   (c) 301867487 2013
-# Licence:     <your licence>
 #-------------------------------------------------------------------------------
 #!/usr/bin/env python
 
-class Record(Object):
+class Record(object):
     #pre: 6 variables are passed
     #post: a record with 6 categories are created
     #purpose: create a record
-    def __init__(self,fname, nname, position, salary, credit, idN ):
+    def __init__(self,fname, lname, position, salary, credit, idN ):
         with(self):
             fname=fname
-            nname=nname
+            lname=lname
             position=position
             salary=salary
             credit=credit
@@ -50,6 +48,20 @@ class Data(object):
     #post: return the index of the record in the list
     #purpose: to search for a certain record
     def search(self, key, value):
+        for i in range(0, len(self._recs), 1):
+            if key=="fname" and self._recs[i].fname==value:
+                return i
+            elif(key=="lname" and self._recs[i].lname==value):
+                return i
+            elif(key=="position" and self._recs[i].position==value):
+                return i
+            elif(key=="salary" and self._recs[i].salary==value):
+                return i
+            elif(key=="credit" and self._recs[i].credit==value):
+                return i
+            elif(key=="idN" and self._recs[i].idN==value):
+                return i
+
 
 
     #pre: no pre condition
@@ -64,13 +76,14 @@ class Data(object):
     #post:
     #purpose:
     def splice(self):
-
+        pass
 
     # pre: a key and a value are passed and must be valid
     # post: return a string that represent the record
     # purpose: to convert the a record to string
     def serialize(self, key, value):
         index=self.search(key, value)
+        s=
 
 
     #pre: a key and a value are passed and must be valid

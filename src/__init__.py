8a5cdb43fa00bfbb694ffacc9b54bcbb826acd4a
Jimmy Li <contact@jimmyzmli.ca>
2013-01-29 23:04:47 -0500

    Organized preliminary test cases

--- src/__init__.py
+++ src/__init__.py
@@ -3,20 +3,36 @@ import ui.all as ui
 import defs as HRP
 
 win = ui.Window(HRP.WIN_TITLE)
-#sys.stdout = win.GetStdOut()
-tpane = ui.TextPanel(win, "text_1", "INTRO TO PROGRAM AAAAAAAAAAAAA"+"...... "*100)
-ttpane = ui.TextPanel(win, "text_2", "PANEL II")
-tttpane = ui.TextPanel(win, "text_2", "PANEL III")
+
+#Test logger
+sys.stdout = win.GetStdOut()
+
+#Create some dummy panels
+tpane = ui.TextPanel("text_1", "INTRO TO PROGRAM AAAAAAAAAAAAA"+"...... "*100, parent=win)
+ttpane = ui.TextPanel("text_2", "PANEL II", parent=win)
+tttpane = ui.TextPanel("text_2", "PANEL III", parent=win)
+
+#Try flipping between panels
 win.add_panel(tpane)
 win.add_panel(ttpane)
 win.show_panel(tpane)
 win.show_panel(ttpane)
 win.add_panel(tttpane,True)
-epane = ui.ElementsPanel(win, "search")
-x = ui.Element()
-x.value = "HELLO"
-x.type = "text"
-x.ratio = 0.5
-epane.add_element(x)
-win.add_panel(epane, True)
+
+#Test InputPanel
+empane = ui.InputPanel("search", parent=win)
+
+#Example data list
+em_prof_p = [
+    ui.Element(type="text", name="First Name", value="", ratio=0.5),
+    ui.Element(type="text", name="Last Name", value="", ratio=0.5),
+    ui.Element(type="select", name="Job Title", value="", opts=["Manager", "Associate", "CEO"], ratio=1),
+    ui.Element(type="text", name="Phone", value="", ratio=1)
+    ]
+
+for x in em_prof_p:
+    empane.add_element(x)
+win.add_panel(empane, True)
+
+#Show result
 ui.launch_win(win)

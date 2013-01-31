4af23a5c04e654de4774b6553b1f0705fb2f65b0
kevinxyc <kevinxyc@qq.com>
2013-01-31 15:02:06 -0500

    added extra field for record class

--- src/record.py
+++ src/record.py
@@ -12,15 +12,15 @@ class Record(object):
     #pre: 6 variables are passed
     #post: a record with 6 categories are created
     #purpose: create a record
-    def __init__(self,fname, lname, position, salary, credit, idN ):
-        with(self):
-            fname=fname
-            lname=lname
-            position=position
-            salary=salary
-            credit=credit
-            idN=idN
-
+    def __init__(self, **kwargs ):
+        #with(self):
+            self.fname= kwargs.pop('fname', "")
+            self.lname= kwargs.pop('lname', "")
+            self.position= kwargs.pop('position', "")
+            self.salary= kwargs.pop('salary', "")
+            self.credit= kwargs.pop('credit', "")
+            self.id= kwargs.pop('id', -1)
+            self._extra = kwargs
 
 
 class Data(object):
@@ -49,6 +49,9 @@ class Data(object):
     #purpose: to search for a certain record
     def search(self, key, value):
         for i in range(0, len(self._recs), 1):
+            #hasattr(obj, attr_name)
+            #getattr(obj, attr_name)
+            #if key in dictionary
             if key=="fname" and self._recs[i].fname==value:
                 return i
             elif(key=="lname" and self._recs[i].lname==value):
@@ -59,7 +62,7 @@ class Data(object):
                 return i
             elif(key=="credit" and self._recs[i].credit==value):
                 return i
-            elif(key=="idN" and self._recs[i].idN==value):
+            elif(key=="id" and self._recs[i].id==value):
                 return i
 
 
@@ -69,7 +72,9 @@ class Data(object):
     #purpose: to show all records
     def showall(self):
         for i in range(0, len(self._recs), 1):
-            print self._recs[i]
+            index=i
+            s= self._recs[index].fname,self._recs[index].lname,self._recs[index].position,self._recs[index].salary,self._recs[index].credit,self._recs[index].id,self._recs[index]._extra
+            print s
 
 
     #pre:
@@ -83,7 +88,8 @@ class Data(object):
     # purpose: to convert the a record to string
     def serialize(self, key, value):
         index=self.search(key, value)
-        s=
+        s= self._recs[index].fname,self._recs[index].lname,self._recs[index].position,self._recs[index].salary,self._recs[index].credit,self._recs[index].id,self._recs[index]._extra
+        return s
 
 
     #pre: a key and a value are passed and must be valid
@@ -91,6 +97,4 @@ class Data(object):
     #purpose: to find a id number of a certain record
     def getid(self, key, value):
         index=self.search(key, value)
-
-
-
+        return self._recs[index].id
